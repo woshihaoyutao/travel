3163a0629eb7bf021f3c129f66c45369ac9089f7
hyt <1151123047@qq.com>
2023-01-14 10:19:58 +0800

    首页轮播图开发完成

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import fastclick from "fastclick";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 import "styles/reset.css";
 import "styles/border.css";
 import "styles/iconfont.css";
 
 Vue.config.productionTip = false;
 fastclick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" /></swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+    name: "HomeSwiper",
+    data() {
+        return {
+            swiperOption: {
+                pagination: ".swiper-pagination",
+                loop: true
+            },
+            swiperList: [
+                {
+                    id: "0001",
+                    imgUrl: "https://www.niuqiqi.com/uploadfile/202112/d114cc19ac1852.jpg"
+                },
+                {
+                    id: "0002",
+                    imgUrl:
+                        "https://www.niuqiqi.com/uploadfile/202112/6d42c79e1d7f15b.jpg"
+                }
+            ]
+        };
+    }
+};
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background:#fff
+.wrapper
+    overflow: hidden
+    width: 100% 
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+        width: 100%
+</style>
