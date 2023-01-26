6bc6283b90d4b1365bb52fa8d3e7d20867ed01f2
hyt <1151123047@qq.com>
2023-01-26 10:04:48 +0800

    封装了一个公共动画组件，渐隐渐现效果,整个项目基本开发完毕

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img
-        class="banner-img"
-        :src="bannerImg"
-      />
+      <img class="banner-img" :src="bannerImg" />
       <div class="banner-info">
         <div class="banner-title">
           {{ this.sightName }}
@@ -15,34 +12,42 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from "common/gallary/Gallary";
+import FadeAnimation from "common/fade/FadeAnimation";
 export default {
   name: "DetailBanner",
-  props:{
-    sightName:String,
-    bannerImg:String,
-    bannerImgs:Array
+  props: {
+    sightName: String,
+    bannerImg: String,
+    bannerImgs: Array
   },
   data() {
     return {
-      showGallary: false,
+      showGallary: false
     };
   },
   methods: {
     handleBannerClick() {
       this.showGallary = true;
     },
-    handleGallaryClose(){
-        this.showGallary = false
+    handleGallaryClose() {
+      this.showGallary = false;
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 };
 </script>
